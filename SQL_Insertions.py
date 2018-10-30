8a3819c55c2dfd846093765a7b395f730780f0bf
Matt <mqunell@gmail.com>
2018-10-29 22:58:15 -0700

    Change all % strings to f-strings

--- SQL_Insertions.py
+++ SQL_Insertions.py
@@ -1,6 +1,6 @@
 # Standardized file writing
 def write_sql(input_list, file_loc):
-    insert = "insert into %s values (" % file_loc[file_loc.index('/')+1:]
+    insert = f"insert into {file_loc[file_loc.index('/')+1:]} values ("
 
     output = ""
 
@@ -8,11 +8,11 @@ def write_sql(input_list, file_loc):
         output += insert
 
         for data in line.split(", "):
-            output += "\n\t%s," % data
+            output += f"\n\t{data},"
 
         output = output[:-1] + "\n);\n\n"
 
-        with open("output/%s_Insertions.sql" % file_loc, "w") as output_file:
+        with open(f"output/{file_loc}_Insertions.sql", "w") as output_file:
             output_file.writelines(output[:-2])
 
 
@@ -27,5 +27,5 @@ data = [("music_artists", "MusicDB/Artist"),
 # Loops through the data pairs, opens each input file, reads each file into a list of strings where each string is a
 # line (besides '\n') of the text file, and passes the list and output location to write_sql()
 for pair in data:
-    with open("input/%s.txt" % pair[0], "r") as input_file:
+    with open(f"input/{pair[0]}.txt", "r") as input_file:
         write_sql(input_file.read().split("\n"), pair[1])
